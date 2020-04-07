941a3ad8348d647b4e3bf54ee260be89320fa865
Karan Singh Sodhi <karanpreetsodhi1997"gmail.com>
2020-04-07 03:45:51 -0400

    hello world

---
+++ covid-19/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  covid-19
+//
+//  Created by Karan Singh Sodhi on 2020-04-07.
+//  Copyright © 2020 Karan Singh Sodhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world and is amyyy");
+        
+    }
+
+
+}
+
---
+++ covid-19Tests/covid_19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  covid_19Tests.swift
+//  covid-19Tests
+//
+//  Created by Karan Singh Sodhi on 2020-04-07.
+//  Copyright © 2020 Karan Singh Sodhi. All rights reserved.
+//
+
+import XCTest
+@testable import covid_19
+
+class covid_19Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
